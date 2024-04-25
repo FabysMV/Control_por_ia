2d69e0a26f7508bb320731b48f72f0d72dbacba4
DiegoZavC <147680362+DiegoZavC@users.noreply.github.com>
2024-04-25 07:25:09 +0000

    Diversos cositos para probar como se mueve
    Perdón si está muy comentado

--- motor/motor.ino
+++ motor/motor.ino
@@ -24,13 +24,49 @@ void loop() {
 //  delay(1500);
 //  stop();
 //  delay(1000);
+//-----------------------------------------
   h();
   delay(4000);
   a_h();
   delay(1500);
   stop();
   delay(1000);
-
+  //-------------------------------------------
+  //_------------------------Test_1: Ambos motores giran en sentido horario juntos durante tres segundos, y se detienen durante 3 segundos
+  h();//¿Hacia donde gira?
+  delay(3000);
+  stop();//¿Qué hace al detenerse, cambia su giro, que tanto?
+  delay(3000);
+  //Al reinciar el giro horario, ¿vuelve a girar como al principio?
+  //----------------------------------------------------------------------------------------------------
+  //-------------------------Test_2: Ambos motores giran en sentido antihorario juntos durante 3 segundos, y se detienen durante 3 segundos
+  a_h();
+  delay(3000);
+  stop();
+  delay(3000);
+  //¿El comportamiento es similar al test_1, solo cambiando la dirección de giro? En principio debería serlo
+  //-------------------------------------------------------------------------------------------------------------
+  //--------------------------------------------------Test_3: Ambos motores giran en sentido horario, ¿que pasa si para frenar intercalamos entre h() y a_h() un numero x de veces?
+  h();
+  delay(3000);
+  for (int i = 1; i==5; i++){
+    a_h();
+    delay(200);
+    h();
+    delay(200);
+  }
+  //¿Cómo se comporta? ¿Se detiene en seco?¿Sigue girando de forma prolongada y se detiene "Suavemente"?¿hay siquiera alguna diferencia con los test anteriores?
+  //¿Variar las veces que se repite el intercalado de los motores hace algo?¿Variar el tiempo corto que hay entre las "oscilaciones" hace algo?
+  //-----------------------------------------------------------------------------------------------------------------------
+  //--------------------------------Test_4: Similar al test 3, direcciones cambiadas, en principio se debe comportar igual, es más que nada un control para ver que no pase nada raro
+  a_h();
+  delay(3000);
+  for (int i = 1; i==5; i++){
+    h();
+    delay(200);
+    a_h();
+    delay(200);
+  }
 }
  
 //void backward()
