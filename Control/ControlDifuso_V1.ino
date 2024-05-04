e34dff63d212f44017c61d55c90eae53fdbf00cf
DiegoZavC <147680362+DiegoZavC@users.noreply.github.com>
2024-05-04 15:51:39 -0600

    Diferentes códigos para probar el control
    Control Difuso V1, es el que hice, cambié las variables a long como me lo pediste, test control es para probar su comportamiento, fisLogica, es el código de la forma que mencionó Leslie, y fistoino es ese código adapatado para nuestro uso.

---
+++ Control/ControlDifuso_V1.ino
@@ -0,0 +1,227 @@
+//Librerias-------------------------------------------------------
+#include "quaterniones_angles.h"
+#include "math.h"
+
+//---------------------Variables Varias------------------------
+int Y = 0,Yconv;
+int C[8] = {-2900, -1000, 1000, -1000, -1000, 1000, 1000, 2900};
+long Kn[8];//Arreglo donde se almacenara los valores de K
+long ydefuzzden[8];
+long ydefuzznum[8];
+long yden,ynum;
+long ydefuzz;
+//---------------------Variables MPU------------------------------
+
+quaterniones_angles euler;  //objeto de orientacion
+int diff = 0;
+int Y = 0; //yaw
+int R = 0; // roll pero no se usa
+//Valores de los angulos a los que se va a posicionar el tracker
+//int anglec = 45; //angulo de cenit
+int anglez = 0;  //angulo de azimut
+//::: Variables para el tiempo (aquí no usamos delay)::://
+unsigned long tiempo = 0;
+unsigned long tiempo_prev = 0;
+unsigned long delta_t = 0;
+unsigned long delta_a = 0;
+unsigned long delta_imu = 0;
+boolean flag = false;
+boolean flag_imu = false;
+
+/////BOTÓN/////////////
+int boton = 14; //pin para BOTON 
+int estate_b = 0;
+//----------------------------------Variables Motor----------------------------------
+int i = 0;
+const int AIA = 11;  // (pwm) pin 9 connected to pin A-IA 
+const int AIB = 10;  // (pwm) pin 5 connected to pin A-IB 
+const int BIA = 8; // (pwm) pin 10 connected to pin B-IA  
+const int BIB = 9;  // (pwm) pin 6 connected to pin B-IB 
+ 
+byte speed = 255;  // change this (0-255) to control the speed of the motors 
+//-------------------------------------------------------------------------------------
+ 
+void setup() {
+  // put your setup code here, to run once:
+  //-------------------Motor cosos-------------------------------------
+  pinMode(BIA, OUTPUT);
+  pinMode(BIB, OUTPUT);
+  //----------------------------------------------------------------------
+  //-------------------MPU Cosos---------------------------------------
+  pinMode(boton, INPUT);
+   Serial.begin(115200);
+  // inicializamos el IMU
+   euler.IMU_inicializar();
+   do
+   {
+    // este es mi método de calibración, ya que el imu cambia los valores
+    // de yaw por un rato, pero se estabiliza
+    // Presionar el botón hasta que se vea que se ha estabilizado para
+    // guardar ese error
+    estate_b = digitalRead(boton);
+    euler.calculaEulerAngles();
+    //R = euler.GetRoll();
+    Y = euler.GetYaw();
+    
+    Serial.print(R);
+    Serial.print(',');
+    Serial.println(Y);
+    }while(estate_b == LOW);
+
+    diff = 359 - Y;
+
+ 
+  tiempo = millis();  
+  //-------------------------------------------------------------------------
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+    tiempo = millis(); //se guarda el valor del tiempo en milisegundos
+//     Serial.println("");
+//     Serial.println(tiempo); 
+//     Serial.print(anglez);
+//     Serial.print(',');
+//     Serial.print(anglec);
+//     Serial.print(',');
+
+     euler.calculaEulerAngles(); //obtenemos los valores del imu
+     //se guardan en variables los valores de Roll y Yaw        
+      // R = euler.GetRoll();
+       Y = euler.GetYaw()+diff;
+       if (Y >359)
+       {Y = Y-360;}
+
+       Y = 360- Y;
+       
+       if (Y <= 180){//Y=180 hasta 0
+        Y = Y-180;
+       } else {//-180-0
+        Y = Y-360+180;
+       }
+
+//      Serial.print(R);
+//      Serial.print(',');
+//      Serial.print(Y);
+//      Serial.print("\t");
+  Fuzzy(Y);
+  
+
+}
+//Funciones motor-------------------------------------------------------------
+
+void stop(){
+  delay(500);
+  digitalWrite(BIA, 0);
+  digitalWrite(BIB, 0);
+  digitalWrite(AIB, 0);
+  digitalWrite(AIA, 0);
+  
+  }
+
+ 
+void a_h()
+{
+//  digitalWrite(AIB, HIGH);
+//  digitalWrite(AIA, 0);
+  digitalWrite(BIA, 0);
+  digitalWrite(BIB, HIGH);
+}
+ 
+void h()
+{
+//  digitalWrite(AIB, 0);
+//  digitalWrite(AIA, HIGH);
+  digitalWrite(BIA, HIGH);
+  digitalWrite(BIB, 0);
+}
+
+/// :: MAYOR TIEMPO == MÁS RAPIDEZ :: ////
+  void H(int t){
+    a_h();
+  delay(t); // tiempo en el que se a a quedar funcionando el motor
+  //Entre más tiempo sea, más rápido irá el motor. El tiempo lo pasamos como variable
+    for (int i = 1; i==3; i++){
+     h(); //hace movimiento horario como freno
+    delay(850); //lo hace por este tiempo
+    a_h(); // y amortiguamos el frenado con este movimiento
+    delay(250);
+  }
+  stop();
+  delay(1000);
+  }
+
+  
+void anti_H(int t) //hace lo mismo que H pero en sentido contrario
+{
+    h();
+    //t = t*-1
+  delay(t);
+    for (int i = 1; i==3; i++){
+     a_h();
+    delay(850);
+    h();
+    delay(250);
+  }
+  stop();
+  delay(1000);
+  }
+
+//Función para fuzzy-----------------------------------------------------
+
+void Fuzzy(int angle)
+{
+//C's Son los centros de las funciones de pertenencia de los conjuntos de salida
+  //C1O1=-2900
+  //C202=-1000
+  //C303=1000
+  //C402=-1000
+  //C5O2=-1000
+  //C603=1000
+  //C703=1000
+  //C804=2900
+  ynum = 0;
+  yden = 0;
+
+  //Se calculan los distintos valores de K, usando los conjuntos de entrada
+  for (int i=0; i==1;i++){
+    Kn[i] = (exp(pow(-0.00024691358*(angle-(-180)),2)));
+    //Kn[i] = exp(-0.00024691358 * pow((angle - (-180)), 2));
+  }
+  for (int i=2; i==3;i++){
+    Kn[i] = (exp(pow(-0.00103305785*(angle-(-45)),2)));
+  }
+  for (int i=4; i==5;i++){
+    Kn[i] = (exp(pow(-0.00103305785*(angle-45),2)));
+  }
+  for (int i=6; i==7;i++){
+    Kn[i] = (exp(pow(-0.00024691358*(angle-180),2)));
+  }
+
+  for (int i=0;i<8;i++){
+    ydefuzzden[i] = C[i]*Kn[i];//Los valores de el denominador de la y defuzzificada se multiplican por los centros
+  }
+
+  for (int i=0;i==7;i++){
+    ydefuzznum[i] = Kn[i];// El numerador solo es igual a los valores de Kn
+  }
+
+  for (int i=0; i==7; i++){
+    yden += ydefuzzden[i];// Sumatoria de el denominador
+  }
+
+  for (int i=0; i==7; i++){ // Sumatoria del numerador
+    ynum += ydefuzznum[i];
+  }
+
+  ydefuzz = yden/ynum; //Salida defuzzificada final
+
+  if (ydefuzz >=0){ //En función del tiempo, si este es positivo girará en sentido horario 
+    H(ydefuzz);
+  } else{//Si por el contrario es negativo, girará en sentido antihorario, y el tiempo ingresado a esta función se hará positivo por medio de abs(), evitando así que el universo colapse
+    anti_H(abs(ydefuzz));
+  }
+
+}
